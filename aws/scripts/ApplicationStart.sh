3941f478e17cb1b1f7a70f56994a22fd2e99d616
davyzhang3 <davyzhang325@gmail.com>
2023-03-29 19:53:04 -0400

    update

---
+++ aws/scripts/ApplicationStart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+sudo -i -u ec2-user bash <<EOF
+echo "Stop and remove container"
+if docker ps -a --format '{{.Names}}' | grep -q '^sklearn_flask$'; then
+    docker stop sklearn_flask && docker rm sklearn_flask
+fi
+echo "Run the container"
+docker run -d -p 8000:8000 --name sklearn_flask ${{ secrets.DOCKERHUB_USERNAME }}/sklearn_flask:latest
+EOF
