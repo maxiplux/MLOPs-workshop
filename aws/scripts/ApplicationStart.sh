302246d20fc3e032c5d9cde05e9bd6d0ccc4c14f
davyzhang3 <davyzhang325@gmail.com>
2023-04-03 17:14:58 -0400

    fix: update the dockerhub username

--- aws/scripts/ApplicationStart.sh
+++ aws/scripts/ApplicationStart.sh
@@ -6,5 +6,5 @@ if docker ps -a --format '{{.Names}}' | grep -q '^sklearn_flask$'; then
     docker stop sklearn_flask && docker rm sklearn_flask
 fi
 echo "Run the container"
-docker run -d -p 8000:8000 --name sklearn_flask ${{ secrets.DOCKERHUB_USERNAME }}/sklearn_flask:latest
+docker run -d -p 8000:8000 --name sklearn_flask junglepolice/sklearn_flask:latest
 EOF
